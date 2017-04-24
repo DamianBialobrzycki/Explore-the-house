4faecd3b035f30283981c51a83f61dbf3c6bac55
Damian Białobrzycki <damianbialobrzycki@gmail.com>
2017-04-24 12:47:58 +0200

    Added classes Location, Outside and Room

---
+++ Explore the house/Outside.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explore_the_house
+{
+    class Outside : Location
+    {
+        public Outside(string name, bool hot) : base(name)
+        {
+            this.hot = hot;
+        }
+
+        private bool hot;
+
+        public override string Description
+        {
+            get
+            {
+                string NewDescription = base.Description;
+                if (hot)
+                    NewDescription += " Tutaj jest bardzo gorąco.";
+                return NewDescription;
+            }
+        }
+    }
+}
